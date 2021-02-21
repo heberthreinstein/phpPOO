b7a946c6251f4c76046e1cf99092e7b9eaa0c87e
Heberth Reinstein <heberth.reinstein@gmaile.com>
2021-02-21 16:55:24 -0300

    Classe carro

---
+++ Carro.php
@@ -0,0 +1,21 @@
+<?php
+class Carro {
+    
+    //Atributos
+    public int $capacidade;
+    public int $kilometragem;
+    public int $combustivel;
+   
+    function andar() {
+        
+    }
+
+    function parar() {
+
+    }
+
+    function abastecer(){
+
+    }
+}
+?>
